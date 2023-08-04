a4a8fedae1c0fa3b003176b9d1d2fba9cf412495
bjorne90 <bjce90@gmail.com>
2023-08-04 00:40:24 +0000

    Initial commit, started react project, adding hooks, adding links to fetch from API backend

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
+import App from './App';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <div className="main-backdrop"></div>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/pages/Signin.js
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import { Button, Card, Col, Form, Row } from "react-bootstrap";
+import { Link, useNavigate } from "react-router-dom";
+
+export default function Signin() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const response = await fetch("http://localhost:8000/api/login/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: email,
+        password,
+      }),
+    });
+  
+    const data = await response.json();
+    if (data.access) {
+      localStorage.setItem("access_token", data.access);
+      localStorage.setItem("refresh_token", data.refresh);
+      localStorage.setItem("user_id", data.user.id); // assuming user id is located in user.id
+      navigate('/dashboard/events');
+    
+    } else {
+      console.error("Invalid email or password");
+    }
+  }  
+
+  return (
+    <div className="page-sign">
+      <Card className="card-sign">
+        <Card.Header>
+          <Link to="/" className="header-logo mb-4">dashbyte</Link>
+          <Card.Title>Logga in</Card.Title>
+          <Card.Text>Välkommen till vårt intranät</Card.Text>
+        </Card.Header>
+        <Card.Body>
+          <Form onSubmit={handleSubmit}>
+            <div className="mb-4">
+              <Form.Label >Epost</Form.Label>
+              <Form.Control type="text" placeholder="Enter your email address" onChange={(e) => setEmail(e.target.value)} />
+            </div>
+            <div className="mb-4">
+              <Form.Label className="d-flex justify-content-between">Lösenord <Link to="">Glömt ditt lösenord?</Link></Form.Label>
+              <Form.Control type="password" placeholder="Enter your password" onChange={(e) => setPassword(e.target.value)} />
+            </div>
+            <Button type="submit" variant="primary" className="btn-sign">Sign In</Button>
+
+            <div className="divider"><span>Saknar du konto? Kontakta oss!</span></div>
+          </Form>
+        </Card.Body>
+      </Card>
+    </div>
+  )
+}
